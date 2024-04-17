f3ea1540830beeccc90eb75f94fc7cbaedf49139
Benjamin Maldonado <benja.ignacio2001@gmail.com>
2024-04-16 22:20:08 -0400

    post BudgetAccount

--- AppPresupuestos.Server/Controllers/BudgetAccountController.cs
+++ AppPresupuestos.Server/Controllers/BudgetAccountController.cs
@@ -12,7 +12,8 @@ namespace Server.Controllers
         {
         }
 
-        [HttpGet()]
+        #region Get
+        [HttpGet]
         public ActionResult<BudgetAccount> Get()
         {
             List<Presupuestos.BudgetAccount> list = Presupuestos.BudgetAccount.GetAll();
@@ -25,5 +26,27 @@ namespace Server.Controllers
             List<Presupuestos.BudgetAccount> list = Presupuestos.BudgetAccount.GetLevels(level);
             return StatusCode(StatusCodes.Status200OK, list);
         }
+        #endregion
+
+        #region Post
+        [HttpPost]
+        public async Task<ActionResult<Presupuestos.BudgetAccount>> Post(Presupuestos.BudgetAccount budgetAccount)
+        {
+            Presupuestos.BudgetAccount model = new Presupuestos.BudgetAccount();
+            using (Presupuestos.Context context = new Presupuestos.Context())
+            {
+                model = new Presupuestos.BudgetAccount
+                {
+                    Id = Guid.NewGuid(),
+                    BudgetAccountId = budgetAccount.BudgetAccountId,
+                    Level = budgetAccount.Level,
+                    Description = budgetAccount.Description
+                };
+                context.Add(model);
+                context.SaveChanges();
+            }
+            return StatusCode(StatusCodes.Status201Created, model);
+        }
+        #endregion
     }
 }
