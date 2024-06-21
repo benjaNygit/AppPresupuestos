82030bb444bedff231a632f0e2018c08fab01b86
Benjamin Maldonado <benja.ignacio2001@gmail.com>
2024-06-20 20:44:54 -0400

    Bussines model Voucher

--- Presupuestos/Business/Voucher.cs
+++ Presupuestos/Business/Voucher.cs
@@ -4,9 +4,45 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Presupuestos.Business
+namespace Presupuestos
 {
     public partial class Voucher
     {
+        public static Voucher? Get(Guid id) => Query.GetVouchers(id).FirstOrDefault<Voucher>();
+        
+        public static List<Voucher> GetAll(Article article)
+        {
+            return (from query in Query.GetVouchers(article)
+                    select query
+                    ).ToList<Voucher>();
+        }
+
+        public static List<Voucher> GetAll(Budget budget)
+        {
+            return (from query in Query.GetVouchers(budget)
+                    select query
+                    ).ToList<Voucher>();
+        }
+
+        public static List<Voucher> GetAll(DateOnly date)
+        {
+            return (from query in Query.GetVouchers(date)
+                    select query
+                    ).ToList<Voucher>();
+        }
+
+        public static List<Voucher> GetAll(DateOnly date, bool before)
+        {
+            return (from query in Query.GetVouchers(date, before)
+                    select query
+                    ).ToList<Voucher>();
+        }
+
+        public static List<Voucher> GetAll(DateOnly startDate, DateOnly endDate)
+        {
+            return (from query in Query.GetVouchers(startDate, endDate)
+                    select query
+                    ).ToList<Voucher>();
+        }
     }
 }
